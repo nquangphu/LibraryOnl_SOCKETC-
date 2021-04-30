b808289b0ca130fa8ff1d5058e9c5f65718529e8
nquangphu <64113119+nquangphu@users.noreply.github.com>
2021-04-30 17:34:32 +0700

    .

--- TH/Client/Client.py
+++ TH/Client/Client.py
@@ -5,8 +5,11 @@ from PIL import Image, ImageTk
 window1 = Tk()
 window1.title("Client")
 window1.geometry("600x400")
+window1.configure(bg = 'pink')
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-######################## IP CONNECT ####################
+
+##############################################################
+#------------------------ IP CONNECT ------------------------#
 def Exit(window):
     window.destroy()
 def Send(data):
@@ -23,28 +26,31 @@ img_tmp = Image.open("image.jpg")
 img_tmp = img_tmp.resize((500, 135), Image.ANTIALIAS)
 img = ImageTk.PhotoImage(img_tmp)
 panel = Label(window1, image = img)
-panel.pack(ipady = 10)
-Label_IP = Label(window1, text = "IP", fg = "blue2", font = "Arial")
-Label_IP.place(x = 130, y = 155)
+panel.pack(pady = 10)
+Label_IP = Label(window1, text = "IP", fg = "navy", font = "Arial")
+Label_IP.place(x = 130, y = 165)
 
 
-Entry_IP = Entry(window1, bd = 3, bg = "AntiqueWhite4", width = 40)
-#Entry_IP.place(x = 50, y = 30
-Entry_IP.pack()
+Entry_IP = Entry(window1, bd = 3, bg = "gainsboro", fg = "red", width = 40)
+Entry_IP.pack(pady = 10)
 
 btn_cnt = Button(window1, text = "Connect", fg = "navy", bg = "snow", width = 10)
-btn_cnt.place(x = 450, y = 155)
+btn_cnt.place(x = 450, y = 165)
 
 btn_cnt["command"] = lambda: Connect(Entry_IP.get())
 
+label_text = Label(window1, text = "Are you sure the connection is successful already?", fg = "navy")
+label_text.place(x = 168, y = 250)
 
 btn_sgin = Button(window1, text = "Sign in", fg = "navy", bg = "snow", width = 10)
-btn_sgin.place(x = 220, y = 220)
+btn_sgin.place(x = 210, y = 280)
 
 btn_sgup = Button(window1, text = "Sign up", fg = "navy", bg = "snow", width = 10)
-btn_sgup.place(x = 300, y = 220)
+btn_sgup.place(x = 310, y = 280)
 
-################ SIGN IN #########################
+
+##############################################################
+#-------------------------- SIGN IN -------------------------#
 def Sub(kind,usn, pwd):
     print(kind + "+" + usn + "+" + pwd)
     Send(kind + "+" + usn + "+" + pwd)
@@ -59,6 +65,12 @@ def SignIn():
     window_sgin = Toplevel(window1)
     window_sgin.title("Sign in")
     window_sgin.geometry("400x200")
+    window_sgin.configure(bg = 'pink')
+    img_sgin = Image.open("signin.jpg")
+    img_sgin = img_sgin.resize((100, 27), Image.ANTIALIAS)
+    imge = ImageTk.PhotoImage(img_sgin)
+    label = Label(window_sgin, image = imge)
+    label.pack(pady = 10)
 
     Label_usn = Label(window_sgin, text = "Username")
     Label_usn.place( x = 40, y = 60)
@@ -72,14 +84,16 @@ def SignIn():
     Entry_pwd = Entry(window_sgin, width = 30, show = '*')
     Entry_pwd.place(x = 110, y = 100)
 
-    btn_submit = Button(window_sgin, text = "Submit", command = lambda: Sub("Signin",Entry_usn.get(), Entry_pwd.get()))
-    btn_submit.place(x = 40, y = 130)
+    btn_submit = Button(window_sgin, text = "Sign in", fg = "navy", bg = "snow", width = 10,
+                         command = lambda: Sub("Signin",Entry_usn.get(), Entry_pwd.get()))
+    btn_submit.place(x = 163, y = 140)
     
     window_sgin.mainloop()
 
-#################### SIGN UP ####################################
 
-def Check_pwd(a,usn, pwd1, pwd2):
+##############################################################
+#-------------------------- SIGN UP -------------------------#
+def Check_pwd(usn, pwd1, pwd2):
     if pwd1 == pwd2:
         Sub("Signup", usn, pwd2)
     else:
@@ -88,35 +102,44 @@ def Check_pwd(a,usn, pwd1, pwd2):
 def SignUp():
     window_sgup = Toplevel(window1)
     window_sgup.title("Sign Up")
-    window_sgup.geometry("400x200")
+    window_sgup.geometry("400x280")
+    window_sgup.configure(bg = 'pink')
+    img_sgup = Image.open("signup.jpg")
+    img_sgup = img_sgup.resize((100, 27), Image.ANTIALIAS)
+    imge = ImageTk.PhotoImage(img_sgup)
+    label = Label(window_sgup, image = imge)
+    label.pack(pady = 10)
     
 
     Label_usn = Label(window_sgup, text = "Username")
-    Label_usn.place( x = 40, y = 30)
+    Label_usn.place( x = 40, y = 70)
 
     Label_pwd = Label(window_sgup, text = "Password")
-    Label_pwd.place( x = 40, y = 70)
+    Label_pwd.place( x = 40, y = 110)
 
     Label_pwd2 = Label(window_sgup, text = "Re-enter password")
-    Label_pwd2.place( x = 40, y = 110)
+    Label_pwd2.place( x = 40, y = 150)
 
     Entry_usn = Entry(window_sgup, width = 30)
-    Entry_usn.place(x = 150, y = 30)
+    Entry_usn.place(x = 150, y = 70)
 
     Entry_pwd = Entry(window_sgup, width = 30, show = '*')
-    Entry_pwd.place(x = 150, y = 70)
+    Entry_pwd.place(x = 150, y = 110)
 
     Entry_pwd2 = Entry(window_sgup, width = 30, show = '*')
-    Entry_pwd2.place(x = 150, y = 110)
+    Entry_pwd2.place(x = 150, y = 150)
 
-    btn_submit = Button(window_sgup, text = "Submit")
-    btn_submit.place(x = 40, y = 160)
+    btn_submit = Button(window_sgup, text = "Sign up", fg = "navy", bg = "snow", width = 10)
+    btn_submit.place(x = 163, y = 200)
     btn_submit['command'] = lambda: Check_pwd(Entry_usn.get(), Entry_pwd.get(), Entry_pwd2.get())
     
     window_sgup.mainloop()
 btn_sgin['command'] = SignIn
 btn_sgup['command'] = SignUp
-############# SEARCH ############################### id ten tacgia  namsx tl 1+tv1+NVA++GK
+
+
+##############################################################
+#-------------------------- SEARCH --------------------------#
 def Sub2(a,b,c,d):
     Send(a + "+" + b + "+" + c + "++" + d)
     data = s.recv(1024).decode("utf-8")
@@ -166,11 +189,14 @@ def window_sch():
     btn_ext['command'] = lambda: Exit(window_sch)
 
     window_sch.mainloop()
-################################# VIEW ############################
-def View(data):        # 10.126.1.174
+
+
+################################################################
+#-------------------------- VIEW BOOK -------------------------#
+def View(data):
     window_view = Tk()
     window_view.title("Viewbook")
-    window_view.geometry("1000x1000")
+    window_view.geometry("1000x800")
     Label_data = Label(window_view, text = data)
     Label_data.grid(padx = 10, pady = 10)
 window1.mainloop()
